a0cab48aecdaca739219ce992408d8759b9c8b63
olili27 <timmolili27@gmail.com>
2025-04-04 16:11:31 +0300

    refactor calculateTax

--- calculator.js
+++ calculator.js
@@ -1,15 +1,24 @@
 /**
- * @param {number} amount - positive integer or float
- * @returns {number}
+ * @param {integer | float} amount
+ * @param {integer | float} taxRate
+ * @returns {integer | float}
+ */
+function calculate(amount, taxRate) {
+  return Number(((amount * taxRate) / 100).toFixed(2));
+}
+
+/**
+ * @param {integer |  float} amount - positive integer or float
+ * @returns {int | float}
  */
 export default function calculateTax(amount) {
   if (amount < 12000) {
-    return 0;
+    return calculate(amount, 0);
   }
 
   if (amount > 36000) {
-    return (40 * amount) / 100;
+    return calculate(amount, 40);
   }
 
-  return (20 * amount) / 100;
+  return calculate(amount, 20);
 }
