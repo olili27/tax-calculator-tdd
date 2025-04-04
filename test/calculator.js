d7a93782a4e4c5a27805c2af2f863aabe646e918
olili27 <timmolili27@gmail.com>
2025-04-04 15:26:12 +0300

    enforce eslint rules

--- test/calculator.js
+++ test/calculator.js
@@ -1,11 +1,11 @@
-import {assert} from "chai";
+import { assert } from "chai";
+import { describe, it } from "mocha";
 import calculateTax from "../calculator.js";
-
 
 describe("Tax Calculator", () => {
   describe("tax calculation with positive numbers", () => {
     it("calculateTax(1) should return 0", () => {
       assert.strictEqual(calculateTax(1), 0);
-    })
+    });
   });
 });
