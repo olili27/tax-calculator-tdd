bf49c3b6b89547a41253056dbb5c2cc97376211c
olili27 <timmolili27@gmail.com>
2025-04-04 15:15:04 +0300

    add testcase with positive numbers
    created calculateTax function, made sure the testcase passes

---
+++ calculator.js
@@ -0,0 +1,8 @@
+/**
+ * 
+ * @param {number} amount - positive integer or float
+ * @returns {number}
+ */
+export default function calculateTax(amount) {
+  return 0;
+}
---
+++ test/calculator.js
@@ -0,0 +1,11 @@
+import {assert} from "chai";
+import calculateTax from "../calculator.js";
+
+
+describe("Tax Calculator", () => {
+  describe("tax calculation with positive numbers", () => {
+    it("calculateTax(1) should return 0", () => {
+      assert.strictEqual(calculateTax(1), 0);
+    })
+  });
+});
